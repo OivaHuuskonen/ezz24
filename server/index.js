e250da30ad6db10530f9b1e5ea738eed23ac0a8d
OivaHuuskonen <Solarsystems3@gmail.com>
2024-05-26 21:37:32 +0300

    b4

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -48,6 +49,64 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Node server is running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+/*import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.js";
+import categoryRoutes from "./routes/category.js";
+import productRoutes from "./routes/product.js";
+import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Check if MONGO_URI is loaded correctly
+if (!process.env.MONGO_URI) {
+  console.error("MONGO_URI is not defined");
+  process.exit(1);
+}
+
+// DB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB ERROR => ", err));
+
+// Middlewares
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Router middleware
+app.use("/api", authRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
+// Serve React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Node server is running on port ${PORT}`);
+});*/
 
 
 
